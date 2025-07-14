07fbf3734644a5eeedbaacba18475fa1d79f8443
TechKillUs <56755556+rmartinguerrero@users.noreply.github.com>
2025-07-14 21:58:29 +0200

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://astro.build/config',
   integrations: [mdx(), sitemap(), tailwind()]
 });
 
